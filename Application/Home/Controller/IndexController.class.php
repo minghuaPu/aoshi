8be3b10bc16d957b8c5dddbae1a3359319c2acbf
smith <3162547163@qq.com>
2016-12-22 11:58:34 +0800

    动态化职位列表

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -16,8 +16,23 @@ class IndexController extends Controller {
 
     public function findjob()
     {
-        // 查询职位列表信息
-        // 赋值到模版变量
+        // 第一步：实例化模型
+       $job= M('job');
+
+       // 第二步：查询职位列表信息
+       
+       $job_info=$job
+                ->field("job.id,job_name,money,company_name")//查询指定的字段
+                ->join("company on job.enterprise_id=company.enterprise_id")//join是关联查询
+                ->select();
+       
+       // echo $job->getLastSql();//获取上一次执行的sql语句
+      
+       // 作关联查询
+
+        // 第三步：赋值到模版变量
+        $this->assign("job_info",$job_info);
+
         $this->display();
     }
 
