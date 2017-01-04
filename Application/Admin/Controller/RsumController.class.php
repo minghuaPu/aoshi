98a4c6f673c957bbb5a114a4277b69c0fd2afe88
smith <3162547163@qq.com>
2017-01-04 17:34:28 +0800

    'angular升级版'

--- Application/Admin/Controller/RsumController.class.php
+++ Application/Admin/Controller/RsumController.class.php
@@ -17,81 +17,32 @@ class RsumController extends Controller {
     	
     	$uid = 1;
 
-    	$getType=I('type');
-    	if ($getType==1) {
-	    		//简历基本信息
-	    	$resumes_basic = M('resumes_basic')->where("uid =$uid")->select();
-	        $this->assign('resumes_basic', $resumes_basic);
+		//简历基本信息
+    	$resumes_basic = M('resumes_basic')->where("uid =$uid")->select();
+    
+		 //简历工作经历
+    	$resumes_jobexp = M('resumes_jobexp')->where("uid =$uid")->select();
+	
+		  //简历教育经历
+    	$resumes_eduexp = M('resumes_eduexp')->where("uid =$uid")->select();
         
-    	}elseif ($getType=='jobexp') {
-    		 //简历工作经历
-	    	$resumes_jobexp = M('resumes_jobexp')->where("uid =$uid")->select();
-	    	echo json_encode($resumes_jobexp);
-    	}else{
-    		  //简历教育经历
-	    	$resumes_eduexp = M('resumes_eduexp')->where("uid =$uid")->select();
-	        $this->assign('resumes_eduexp', $resumes_eduexp);
-	        
-	        //简历求职意向
-	        $resumes_career = M('resumes_career')->where("uid =$uid")->select();
-	        $this->assign('resumes_career', $resumes_career);
-    	}
+        //简历求职意向
+        $resumes_career = M('resumes_career')->where("uid =$uid")->select();
     	
        
-        
+    	echo json_encode(
+		    			array(
+		    					'basic'=>$resumes_basic,
+		    					'jobexp'=>$resumes_jobexp,
+		    					'eduexp'=>$resumes_eduexp,
+		    					'career'=>$resumes_career,
+		    				)
+		    			);
+
       
 
     }
-    public function add() {
-    	
-    	$uid = 1;
     
-   		//简历添加
-   		if(I('index') == "basic"){
-			$resumes_basic = M('resumes_basic');
-			$data['uid'] = $uid;
-			$data['name'] = I('name');
-			$data['intro'] = I('intro');
-			$data['sex'] = I('sex');
-			$data['birth'] = I('birth');
-			$data['xl'] = I('xl');
-			$data['email'] = I('email');
-			$data['gzjy'] = I('gzjy');
-			$data['city'] = I('city');
-			$data['phone'] = I('phone');
-			$data['email'] = I('email');
-			$resumes_basic->data($data)->add();
-			echo true;
-   		} elseif(I('index') == "jobexp") {
-   			$resumes_jobexp = M('resumes_jobexp');
-			$data['uid'] = $uid;
-			$data['name'] = I('name');
-			$data['job'] = I('job');
-			$data['time'] = I('time');
-			$data['cont'] = I('cont');
-			$resumes_jobexp->data($data)->add();
-			echo true;
-   		} elseif(I('index') == "eduexp") {
-			$resumes_eduexp = M('resumes_eduexp');
-			$data['uid'] = $uid;
-			$data['name'] = I('name');
-			$data['major'] = I('major');
-			$data['xl'] = I('xl');
-			$data['grad'] = I('grad');
-			$resumes_eduexp->data($data)->add();
-			echo true;
-   		} elseif(I('index') == "career") {
-   			$resumes_career = M('resumes_career');
-			$data['uid'] = $uid;
-			$data['name'] = I('name');
-			$data['type'] = I('type');
-			$data['city'] = I('city');
-			$data['wages'] = I('wages');
-			$resumes_career->data($data)->add();
-			echo true;
-   		}
-    }
-
 	public function update() {
 		
     	$uid = 1;
@@ -120,9 +71,14 @@ class RsumController extends Controller {
 			$data['job'] = I('job');
 			$data['time'] = I('time');
 			$data['cont'] = I('cont');
-			$resumes_jobexp->where("jid=$jid")->save($data);
 
-			echo true;
+			if ($jid>0) {
+				$rtn_data=$resumes_jobexp->where("jid=$jid")->save($data);
+			}else{
+				$rtn_data=$resumes_jobexp->data($data)->add();
+			}
+
+			echo json_encode(array('msg'=>$rtn_data));
    		} elseif(I('index') == "eduexp") {
    			$eid = I('eid');
    			$resumes_eduexp = M('resumes_eduexp');
@@ -131,8 +87,12 @@ class RsumController extends Controller {
 			$data['xl'] = I('xl');
 			$data['major'] = I('major');
 			$data['grad'] = I('grad');
-			$resumes_eduexp->where("eid=$eid")->save($data);
-			echo true;
+			if ($eid>0) {
+				$rtn_data=$resumes_eduexp->where("eid=$eid")->save($data);
+			}else{
+				$rtn_data=$resumes_eduexp->data($data)->add();
+			}
+			echo json_encode(array('msg'=>$rtn_data));
    		} elseif(I('index') == "career") {
    			$resumes_career = M('resumes_career');
 			$data['uid'] = $uid;
